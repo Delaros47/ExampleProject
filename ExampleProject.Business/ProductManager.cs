7b16ee5ab2cbf01956c085eeb8b745b6e90cf714
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-02-28 18:55:57 +0300

    Add project files.

---
+++ ExampleProject.Business/ProductManager.cs
@@ -0,0 +1,22 @@
+﻿using ExampleProject.Access;
+using ExampleProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleProject.Business
+{
+    public class ProductManager
+    {
+
+
+        public List<Product> GetAll()
+        {
+            ProductDal productDal = new ProductDal();
+            return productDal.GetAll();
+        }
+
+    }
+}
---
+++ ExampleProject.ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using ExampleProject.Business;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ProductManager productManager = new ProductManager();
+foreach (var product in productManager.GetAll())
+{
+    Console.WriteLine(product.ProductName);
+}
+
+
+
+
+
+
+
+
+Console.ReadLine();
