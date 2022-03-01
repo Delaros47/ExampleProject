c63f7f8132bcfc4d34692054855fab2e985b3087
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-03-01 15:04:12 +0300

    ..

---
+++ ExampleProject.Business/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using ExampleProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleProject.Business
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        void Add(Product product);
+    }
+}
